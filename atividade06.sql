2dc958736bcd2187f82b7d73a86f83d0a93bf34b
Joao Victor <102428176+JvMSan@users.noreply.github.com>
2023-07-19 22:41:12 -0300

    finish - 06

--- atividade06.sql
+++ atividade06.sql
@@ -1,3 +1,6 @@
+-- Questão proposta: Liste os alunos matriculados em uma disciplina específica, juntamente com suas respectivas notas finais e o nome do professor que ministrou a disciplina.
+
+-- Consulta SQL proposta:
 SELECT aluno.matricula AS matricula_aluno, disciplina.codigo AS codigo_disciplina,matricula.notaFinal as m_final
 FROM aluno 
 JOIN matricula ON aluno.id = matricula.idAluno
